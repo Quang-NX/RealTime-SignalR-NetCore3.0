a0e8b0c404132543bbf7cae09d52022310dc1596
Quang Nguyễn <adquang199x@gmail.com>
2020-04-03 12:30:04 +0700

    Add project files.

---
+++ RealTime-SignalR-NetCore3.0/SignalR/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealTime_SignalR_NetCore3._0.SignalR
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
